b5846da0edbc41451ac4662d9ebf8cfe017504ef
MasahiroHayashi <catfish.m@nifty.com>
2019-12-25 19:56:05 +0900

    make website

---
+++ js/header_footer.js
@@ -0,0 +1,35 @@
+function makeHeaderFooter(){
+	var header_txt = (function() {/*
+
+	<!-- ヘッダー部分（メニュー等）を修正したい場合は，以下を修正すること。 -->
+	<a href="https://kyoyasai.mirko.jp/"><img src="img/title.jpg" id="titleimg"></a>
+	<ul id="fade-in" class="dropmenu">
+	  <li class="liClassA"><a href="https://kyoyasai.mirko.jp/" style="text-decoration: none;"><span>ＴＯＰ</span></a></li>
+	  <li class="liClassB"><a href="battle.html" style="text-decoration: none;"><span>京育カードバトル</span></a></li>
+	  <li class="liClassB"><a href="ar.html" style="text-decoration: none;"><span>京野菜ＡＲ</span></a></li>
+	  <li class="liClassB"><a href="card.html" style="text-decoration: none;"><span>カード一覧</span></a></li>
+	  <li class="liClassB"><a href="link.html" style="text-decoration: none;"><span>関連リンク集</span></a></li>
+	</ul>
+	<!-- ヘッダー部分ここまで -->
+
+	*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\n|\r/g, "");
+	
+	var footer_txt = (function() {/*
+
+	<!-- フッター部分を修正したい場合は，以下を修正すること。 -->
+
+	<hr style="height: 1px;  border: none;  border-top: 15px #CCFF66 solid;  margin: 20px 0 20px 0;">
+	<!-- 所属情報 -->
+	<div style="margin:0 10px 0 15px; padding-bottom:30px;">
+	<div style="margin:0 0 0 0; font-size:large; font-weight:bold;text-align:center;">&copy; アーバンデータチャレンジ2019京都府ブロック　チーム「<span style="color:#660033;">Kyoto Lovers</span>」</div>
+	<div style="margin:20px 0 0 0;text-align:center;font-size:14px;">お問い合わせは<a href="">こちら</a></div>
+	<div style="margin:20px 0 0 0; font-size:large; font-weight:bold;text-align:center;"><img src="img/lovers.png" width="300px"></div>
+	</div>
+
+	<!-- フッター部分ここまで -->
+
+	*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\n|\r/g, "");
+	document.getElementById('header-box').innerHTML = header_txt ;
+	document.getElementById('footer-box').innerHTML = footer_txt ;
+	document.getElementById('parent').style.display = 'block'; //ヘッダーとフッターを挿入してから最後にペアレントを表示
+}
---
+++ js/modal2.js
@@ -0,0 +1,94 @@
+﻿var header;
+var data;
+
+function callYasaiData() { 
+	var fileName = "data/yasai2.csv";
+	var fileData = readTextFile(fileName) ;
+	var csvData = fileData;
+	csvData = csvData.replace(/\r\n+$/g,''); //終端に改行コード（CR+LF）があれば削除
+	var lines = csvData.split("\r\n");
+	var result = [];
+	var headers = lines[0].split(",");
+	for(var i=1;i<lines.length;i++){
+		var obj = {};
+		var currentline=lines[i].split(",");
+		for(var j=0;j<headers.length;j++){
+			obj[headers[j]] = currentline[j];
+		}
+		result.push(obj);
+	}
+	
+	header = headers; //グローバル変数に代入
+	data = result; //グローバル変数に代入
+}
+function readTextFile(file) { 
+	var rawFile = new XMLHttpRequest(); 
+	rawFile.open("GET", file, false); 
+	rawFile.send(null);
+	return rawFile.responseText;
+} 
+
+function openModal(num){
+	var kText = "";
+	kText += "<div style='text-align:right;'><a class='button-link' href='javascript:void(0);' onclick='closeModal();'>×</a></div>";
+	kText += "<table class='modal_table'>";	
+	for(let i=0 ; i<header.length ; i++) {
+		kText += "<tr><th>" + header[i] + "</th><td>" + data[num][header[i]] + "</td></tr>";
+	}
+	kText += "</table>"
+	document.getElementById('modal-content').innerHTML = kText ;
+
+	//キーボード操作などにより、オーバーレイが多重起動するのを防止する
+	$( this ).blur() ;	//ボタンからフォーカスを外す
+	if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
+	//if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+
+	//オーバーレイを出現させる
+	$( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+	$( "#modal-overlay" ).fadeIn( "fast" ) ;
+
+	//コンテンツをセンタリングする
+	centeringModalSyncer() ;
+
+	//コンテンツをフェードインする
+	$( "#modal-content" ).fadeIn( "fast" ) ;
+
+	//[#modal-overlay]、または[#modal-close]をクリックしたら…
+	$( "#modal-overlay,#modal-close" ).unbind().click( function(){
+
+		//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+		$( "#modal-content,#modal-overlay" ).fadeOut( "fast" , function(){
+
+			//[#modal-overlay]を削除する
+			$('#modal-overlay').remove() ;
+		} ) ;
+	} ) ;
+}
+
+function closeModal(){
+		//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+		$( "#modal-content,#modal-overlay" ).fadeOut( "fast" , function(){
+
+			//[#modal-overlay]を削除する
+			$('#modal-overlay').remove() ;
+		} ) ;
+}
+window.onresize = centeringModalSyncer();
+
+//センタリングを実行する関数
+function centeringModalSyncer(){
+
+		//画面(ウィンドウ)の幅、高さを取得
+		var w = $( window ).width() ;
+		var h = $( window ).height() ;
+
+		// コンテンツ(#modal-content)の幅、高さを取得
+		// jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+		var cw = $( "#modal-content" ).outerWidth();
+		var ch = $( "#modal-content" ).outerHeight();
+		
+		//センタリングを実行する
+		$( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+		//$( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": (15) + "px"} ) ;
+}
+
