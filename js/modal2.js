bae8eb75554e8cb83298ffbd99ee7efe9e12b31e
MasahiroHayashi <catfish.m@nifty.com>
2020-01-16 20:08:44 +0900

    Update modal2.js

--- js/modal2.js
+++ js/modal2.js
@@ -35,7 +35,9 @@ function openModal(num){
 	kText += "<table class='modal_table'>";	
 	for(let i=0 ; i<header.length ; i++) {
 		if(data[num][header[i]].slice(-3).toLowerCase() == "jpg" || data[num][header[i]].slice(-3).toLowerCase() == "png"){
-			urlcode = "<img src='" + data[num][header[i]] + "'>" ;
+			
+			urlcode = "<div style='text-align:center;'><img src='" + data[num][header[i]] + "' style='width:50%;'></div>" ;
+			
 		}else if(data[num][header[i]].slice(0,4) == "http"){
 			urlcode = "<a href='" + data[num][header[i]] +"' target='_blank'>" + data[num][header[i]] + "</a>" ;
 		}else{
