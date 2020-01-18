cef5659236d878f0c32e18fafa919c140ef75a2e
MasahiroHayashi <catfish.m@nifty.com>
2020-01-18 15:52:09 +0900

    update

--- js/hanbaimap.js
+++ js/hanbaimap.js
@@ -1,5 +1,5 @@
 ﻿
-function callHanbaiData()() { 
+function callHanbaiData() { 
 	var fileName = "data/hanbai.csv";
 	var fileData = readTextFile2(fileName) ;
 	var csvData = fileData;
@@ -28,7 +28,7 @@ function readTextFile2(file) {
 function callMap(headers,result) { 	
 
 	<!-- 地図の表示 -->
-	var map = L.map('map').setView([35.017649, 135.779191], 15);
+	var map = L.map('map').setView([35.014168, 135.748239], 12);
 	var gsi = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
 		attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
 	});
@@ -42,7 +42,26 @@ function callMap(headers,result) {
 	L.control.layers(baseMaps).addTo(map);
 	gsi.addTo(map);
 	map.addLayer(gsi);
-	L.marker([35.017649, 135.779191]).bindPopup("ここは僕の家").addTo(map);
-	L.marker([35.017590, 135.778027]).bindPopup("ここは熊野神社").addTo(map);
-	L.marker([35.019137, 135.776634]).bindPopup("ここは京大病院").addTo(map);
+	
+	var text = "";
+	for(var i=0;i<result.length;i++){
+		
+		text = "店名：" + result[i]["店名"] + "<br>";
+		text += "販売先住所：" + result[i]["販売先住所"] + "<br>";
+		text += "販売形態：" + result[i]["販売形態"] + "<br>";
+		text += "備考：" + result[i]["備考"] + "<br>";
+		text += "緯度：" + result[i]["緯度"] + "<br>";
+		text += "経度：" + result[i]["経度"] ;
+		
+		if(result[i]["緯度"] != ""){
+			L.marker([Number(result[i]["緯度"]), Number(result[i]["経度"])]).bindPopup(text).addTo(map);
+		}
+		
+	}
+
+	//L.marker([35.017649, 135.779191]).bindPopup("ここは僕の家").addTo(map);
+	//L.marker([35.017590, 135.778027]).bindPopup("ここは熊野神社").addTo(map);
+	//L.marker([35.019137, 135.776634]).bindPopup("ここは京大病院").addTo(map);
+
+
 }
